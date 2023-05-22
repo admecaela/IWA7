232be242d6bd778e65f23193f3aae823d0c678b1
Mecaela <127962504+admecaela@users.noreply.github.com>
2023-05-22 22:31:10 +0200

    Add files via upload

---
+++ 7.4_challenge2.js
@@ -0,0 +1,12 @@
+const nickname = "Timmy";
+const firstname = "Timothy";
+
+if (nickname) {
+  console.log(`Good morning, ${nickname}!`);
+}
+if (firstname) {
+  console.log(`Good morning, ${firstname}!`);
+}
+else {
+  console.log("Good morning!");
+}
